d712b75856f9adb09b938f92cefeaf2571c0202c
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-14 13:51:56 +0100

    oppgave-3

--- OppgaverIfElse/Program.cs
+++ OppgaverIfElse/Program.cs
@@ -10,6 +10,11 @@ namespace Application
 
         Console.WriteLine($"Oppgave2: Return value with arguments 5 and 6 is {Oppgave2(5,6)}");
         Console.WriteLine($"Oppgave2: Return value with arguments 5 and 5 is {Oppgave2(5, 5)}");
+
+        Console.WriteLine($"Oppgave3: 5, 10 or the sum of 5 + 10 is 30 = {Oppgave3(5,10)}");
+        Console.WriteLine($"Oppgave3: 30, 10 or the sum of 30 + 10 is 30 = {Oppgave3(30, 10)}");
+        Console.WriteLine($"Oppgave3: 10, 20 or the sum of 10 + 20 is 30 = {Oppgave3(10, 20)}");
+
     }
     static bool Oppave1(int x, int y)
     {
@@ -31,6 +36,17 @@ namespace Application
       else
       {
           return x + y;
+      }
+    }
+    static bool Oppgave3(int x, int y)
+    {
+      if ((x == 30) | (y == 30) | (x + y == 30))
+      {
+          return true;
+      }
+      else
+      {
+          return false;
       }
     }
   }
