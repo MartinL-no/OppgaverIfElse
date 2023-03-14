081c509dc2e05ab1cab7327d6185389a154c64b8
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-14 13:51:12 +0100

    oppgave-2

--- OppgaverIfElse/Program.cs
+++ OppgaverIfElse/Program.cs
@@ -7,16 +7,30 @@ namespace Application
     static void Main(string[] args)
     {
         Console.WriteLine($"Oppgave1: 1 equals 2 is {Oppave1(1, 2)}");
+
+        Console.WriteLine($"Oppgave2: Return value with arguments 5 and 6 is {Oppgave2(5,6)}");
+        Console.WriteLine($"Oppgave2: Return value with arguments 5 and 5 is {Oppgave2(5, 5)}");
     }
     static bool Oppave1(int x, int y)
     {
+        if (x == y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static int Oppgave2(int x, int y)
+    {
       if (x == y)
       {
-          return true;
+          return x * y;
       }
       else
       {
-          return false;
+          return x + y;
       }
     }
   }
